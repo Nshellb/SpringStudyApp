4d291329e87f59ca153bdbf2d4c96aa437b37701
Nshellb <nshellb@gmail.com>
2023-03-17 10:36:25 +0900

    GENERATE: 프로젝트 최초생성

---
+++ src/main/java/com/nshellb/springstudyapp/SpringStudyAppApplication.java
@@ -0,0 +1,13 @@
+package com.nshellb.springstudyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nshellb/springstudyapp/SpringStudyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nshellb.springstudyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
